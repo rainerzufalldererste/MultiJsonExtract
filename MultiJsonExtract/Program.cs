9ae5ee3193b557b7a1a33a27f077abca69fd6eac
Christoph Stiller <c.stiller@live.de>
2021-02-19 15:39:56 +0100

    Whatever was done

--- MultiJsonExtract/Program.cs
+++ MultiJsonExtract/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MultiJsonExtract
 {
@@ -10,6 +13,46 @@ namespace MultiJsonExtract
     {
         static void Main(string[] args)
         {
+            var contents = from x in Directory.EnumerateFiles(args[0], args[1]).OrderBy(q => q) select File.ReadAllText(x).Trim(' ', '\n', '\r', '\t');
+
+            foreach (var x in contents)
+            {
+                try
+                {
+                    JToken current;
+
+                    try
+                    {
+                        current = JObject.Parse(x);
+                    }
+                    catch
+                    {
+                        current = JArray.Parse(x);
+                    }
+
+                    for (int i = 2; i < args.Length; i++)
+                    {
+                        if (args[i] == "[")
+                        {
+                            i++;
+
+                            int index = int.Parse(args[i]);
+
+                            current = ((JArray)current)[index];
+                        }
+                        else
+                        {
+                            current = ((JObject)current)[args[i]];
+                        }
+                    }
+
+                    Console.Write(current.ToString() + "; ");
+                }
+                catch
+                {
+                    Console.Write("ERR0R; ");
+                }
+            }
         }
     }
 }
